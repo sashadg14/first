83a15efd8142ed47d21d26c4471a866ae4e53577
alex <mamayko.1998@mail.ru>
2015-11-02 23:14:05 +0400

    Polet perviy

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,6 +6,8 @@
 #include <conio.h>
 #include <string>
 #include <iostream>
+#include <windows.h>
+
 using namespace std;
 
 
@@ -14,54 +16,45 @@ int _tmain(int argc, _TCHAR* argv[])
 	char ch;
 	int code;
 	char s[256][256];
-	int i, j, a = 10, b = 10, I, x, y;
+	int i, j, a = 10, b = 10, I, x, y,l=0;
 	int k = 0;
-	x = a - 1;
-	y = b - 1;
+	x = 3- 1;
+	y = 4 - 1;
+
+
 	while (1)
 	{
-	ch = _getch();
-	code = static_cast<int>(ch);
-	//-------------------------------------------
-
-	if (ch == 'w'){
-	x = x - 1;
-	k = 0;
-	system("cls");
-	}
-	if (ch == 's'){
-	x = x + 1;
-	k = 1;
-	system("cls");
-	}
-	if (ch == 'a'){
-	y = y - 1;
-	k = 2;
-	system("cls");
-	}
-	if (ch == 'd'){
-	y = y + 1;
-	k = 3;
-	system("cls");
-	}
+		goto metka;
+		metka1:
+		l = 1;
+		goto metka2;
+		metka:
+		ch = _getch();
+		code = static_cast<int>(ch);
+		//-------------------------------------------
+		if (ch == 'f')
+			l = 1;
+		metka2:
+		if (l == 1){
+			x = x + 1;system("timeout/t 0.9 ");
+			system("cls");
+			
+			
+		}
 	//------------------------------------------------------------------
 	for (i = 0; i < a; i++)
 	{
 		for (j = 0; j < b; j++)
 		{
 			s[i][j] = ' ';
-			s[x][y] = '0';
-			if (k == 0)
-				s[x - 1][y] = '|';
-			if (k == 1)
-				s[x + 1][y] = '|';
-			if (k == 2)
-				s[x][y - 1] = '=';
-			if (k == 3)
-				s[x][y + 1] = '=';
+			if (l == 1)
+				s[x][y] = '*';
+
+
 			cout << s[i][j];
 		}cout << endl;
-	}
+	}if (l = 1)
+		goto metka1;
 }
 
 //system("pause");
